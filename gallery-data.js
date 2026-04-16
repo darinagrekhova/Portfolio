12a8fa313de4ef5dc4d4fed798b18ee2dc2a952f
Darina Grekhova <darinagrekhova@gmail.com>
2026-04-16 20:04:36 +0300

    Add foreigner artworks to gallery data

--- gallery-data.js
+++ gallery-data.js
@@ -39,6 +39,54 @@ const SERIES = {
       title: "fun for fun 5",
       meta: "oil on canvas<br>30×40 cm"
     }
-  ]
+  ],
 
+  foreigners: [
+
+  {
+      src: "images/foreigner/foreigner-I.jpg",
+      title: "foreigner I",
+      meta: "oil on Inkjet paper<br>29×21 cm"
+    },
+    {
+      src: "images/foreigner/foreigner-II.jpg",
+      title: "foreigner II",
+      meta: "oil on Inkjet paper<br>29×21 cm"
+    },
+    {
+      src: "images/foreigner/foreigner-III.jpg",
+      title: "foreigner III",
+      meta: "oil on Inkjet paper<br>29×21 cm"
+    },
+    {
+      src: "images/foreigner/foreigner-IV.jpg",
+      title: "foreigner IV",
+      meta: "oil on Inkjet paper<br>29×21 cm"
+    },
+    {
+      src: "images/foreigner/foreigner-V.jpg",
+      title: "foreigner V",
+      meta: "oil on Inkjet paper<br>29×21 cm"
+    },
+    {
+      src: "images/foreigner/foreigner-VI.jpg",
+      title: "foreigner VI",
+      meta: "oil on Inkjet paper<br>29×21 cm"
+    },
+    {
+      src: "images/foreigner/foreigner-VII.jpg",
+      title: "foreigner VII",
+      meta: "oil on Inkjet paper<br>29×21 cm"
+    },
+    {
+      src: "images/foreigner/foreigner-VIII.jpg",
+      title: "foreigner VIII",
+      meta: "oil on Inkjet paper<br>29×21 cm"
+    },
+    {
+      src: "images/foreigner/foreigner-IX.jpg",
+      title: "foreigner IX",
+      meta: "oil on canvas<br>70×90 cm"
+    }
+]
 };
