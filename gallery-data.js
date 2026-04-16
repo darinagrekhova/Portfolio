721eff2e8de9f7b0ab7886ed584711c7c6489ec3
Darina Grekhova <darinagrekhova@gmail.com>
2026-04-16 19:28:03 +0300

    Add gallery data for art series

---
+++ gallery-data.js
@@ -0,0 +1,24 @@
+const SERIES = {
+
+  quiexspectat: [
+    {
+      src: "images/qui-exspectat/ladder.jpg",
+      title: "ladder",
+      meta: "oil on canvas<br>50×60 cm"
+    },
+    {
+      src: "images/qui-exspectat/foreignerX.jpg",
+      title: "quid agis",
+      meta: "oil on canvas<br>50×60 cm"
+    }
+  ],
+
+  notnotfun: [
+    {
+      src: "images/nnf/fun-for-fun-1.jpg",
+      title: "fun for fun 1",
+      meta: "oil on canvas<br>30×40 cm"
+    }
+  ]
+
+};
