90803ba171e858942c04bdccc7476946895ebec2
Darina Grekhova <darinagrekhova@gmail.com>
2026-04-16 19:35:11 +0300

    Update sidebar links to point to gallery series

--- sidebar.js
+++ sidebar.js
@@ -5,18 +5,17 @@ function loadSidebar() {
   container.innerHTML = `
 <div class="sidebar">
   <a href="/index.html" class="brand">
-  <img src="images/logo.jpg" alt="logo" class="logo" />
     <div class="name">darina grekhova</div>
   </a>
 
-  <div class="menu-item"><a href="/quiexspectat.html">qui exspectat</a></div>
-  <div class="menu-item"><a href="/notnotfun.html">notnotfun</a></div>
-  <div class="menu-item"><a href="/foreigners.html">foreigners</a></div>
-  <div class="menu-item"><a href="/volante.html">volante</a></div>
-  <div class="menu-item"><a href="/slavic-myth-on-canvas.html">slavic myth on canvas</a></div>
-  <div class="menu-item"><a href="/slavic-myth-on-paper.html">slavic myth on paper</a></div>
-  <div class="menu-item"><a href="/selected-independent.html">selected independent</a></div>
-  <div class="menu-item"><a href="/drawings.html">drawings</a></div>
+  <div class="menu-item"><a href="/gallery.html?series=quiexspectat">qui exspectat</a></div>
+  <div class="menu-item"><a href="/gallery.html?series=notnotfun">notnotfun</a></div>
+  <div class="menu-item"><a href="/gallery.html?series=foreigners">foreigners</a></div>
+  <div class="menu-item"><a href="/gallery.html?series=volante">volante</a></div>
+  <div class="menu-item"><a href="/gallery.html?series=slavic-myth-on-canvas">slavic myth on canvas</a></div>
+  <div class="menu-item"><a href="/gallery.html?series=slavic-myth-on-paper">slavic myth on paper</a></div>
+  <div class="menu-item"><a href="/gallery.html?series=selected-independent">selected independent</a></div>
+  <div class="menu-item"><a href="/gallery.html?series=drawings">drawings</a></div>
 
   <div class="menu-item"><a href="/cv.html">cv</a></div>
   <div class="menu-item"><a href="/contact.html">contacts</a></div>
@@ -27,10 +26,11 @@ function loadSidebar() {
 }
 
 function highlightActiveLink() {
-  const file = window.location.pathname.split("/").pop();
+  const params = new URLSearchParams(window.location.search);
+  const series = params.get("series");
 
   document.querySelectorAll(".menu-item a").forEach(link => {
-    if (link.getAttribute("href").includes(file)) {
+    if (series && link.href.includes(series)) {
       link.classList.add("active");
     }
   });
