59d20b0744ed2d4be237b5dd59d7d1a66a281806
Darina Grekhova <darinagrekhova@gmail.com>
2026-04-16 19:49:01 +0300

    Add additional images to gallery data

--- gallery-data.js
+++ gallery-data.js
@@ -18,6 +18,26 @@ const SERIES = {
       src: "images/nnf/fun-for-fun-1.jpg",
       title: "fun for fun 1",
       meta: "oil on canvas<br>30×40 cm"
+    },
+    {
+      src: "images/nnf/fun-for-fun-2.jpg",
+      title: "fun for fun 2",
+      meta: "oil on canvas<br>30×40 cm"
+    },
+    {
+      src: "images/nnf/fun-for-fun-3.jpg",
+      title: "fun for fun 3",
+      meta: "oil on canvas<br>30×40 cm"
+    },
+    {
+      src: "images/nnf/fun-for-fun-4.jpg",
+      title: "fun for fun 4",
+      meta: "oil on canvas<br>30×40 cm"
+    },
+    {
+      src: "images/nnf/fun-for-fun-5.jpg",
+      title: "fun for fun 5",
+      meta: "oil on canvas<br>30×40 cm"
     }
   ]
 
